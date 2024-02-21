667eae88d25e51220b33690e446fed2c2bad1a7e
mhosan <mhosan@gmail.com>
2024-02-21 12:48:02 -0300

    repo clonado

--- obj/Release/netcoreapp3.1/mhTestApi.AssemblyInfo.cs
+++ obj/Release/netcoreapp3.1/mhTestApi.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("mhTestApi")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f4e2b7cf2124479f67a10a94e8c8a291a84106a4")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b2582e6b54b2c9e55a71c3089ade2dc954800c0d")]
 [assembly: System.Reflection.AssemblyProductAttribute("mhTestApi")]
 [assembly: System.Reflection.AssemblyTitleAttribute("mhTestApi")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
